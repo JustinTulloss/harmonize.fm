e5d22afa1d0c542132bb1500014f62ef1e1d0e95
justin <none@none>
2007-10-10 01:40:02 -0500

    [svn] forgot profile class
    --HG--
    branch : svn.rubiconmusicplayer.com

---
+++ trunk/appserver/masterapp/masterapp/lib/profile.py
@@ -0,0 +1,16 @@
+"""
+Module to provide user interface configuration parameters.
+Will probably eventually become a factory for user-configurable
+configuration, but for now it's just a place to store constants.
+"""
+
+class Profile:
+    border = "#6178A0"
+    queue_bg = "#A0B5DF"
+    primary_txt = "#333"
+    queue_back_txt = None
+    queue_accents = None
+    click = None
+    oddrow_bg = "#FFF6DA"
+    sortoddrow_bg = "#FFDD9E"
+ 
