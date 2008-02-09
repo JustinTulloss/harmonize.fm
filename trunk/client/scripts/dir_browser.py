491f4fa79c72b5416d8fa74356ed756185bc5e30
ohohvi <none@none>
2008-02-09 03:11:43 -0600

    [svn] Dir browser for the uploader is now way cooler, automatically expands and scrolls to your last selection
    --HG--
    branch : svn.rubiconmusicplayer.com

--- trunk/client/scripts/dir_browser.py
+++ trunk/client/scripts/dir_browser.py
@@ -14,6 +14,10 @@ def contains_dir(dir_path):
 
 def get_dir_listing(c):
 	request_dir = c.post_query['node'][0]
+	node_list = get_dir_listing_aux(request_dir)
+	return simplejson.dumps(node_list)
+
+def get_dir_listing_aux(request_dir):
 	new_dirs = [dir for dir in os.listdir(request_dir) if path.isdir(path.join(request_dir, dir))]
 
 	node_list = []
@@ -24,6 +28,22 @@ def get_dir_listing(c):
 		new_node['id'] = dir_path
 		if not contains_dir(dir_path):
 			new_node['leaf'] = True
+		if auto_expand(dir_path):
+			new_node['children'] = get_dir_listing_aux(dir_path)
+			new_node['attributes'] = {'auto_expand':True}
+		elif dir_path == get_default_path():
+			new_node['attributes'] = {'select':True}
 		node_list.append(new_node)
+	return node_list
 
-	return simplejson.dumps(node_list)
+def auto_expand(dir_path):
+	final_path = get_default_path()
+	return final_path.startswith(dir_path, 0, len(final_path)-1)
+
+#This is just copied from scripts/upload.py
+def get_default_path():
+	paths = {'posix':os.getenv('HOME'),
+		'nt':r'C:\\',
+		'mac':path.join(os.getenv('HOME'), 'Music')}
+	
+	return paths[os.name]
