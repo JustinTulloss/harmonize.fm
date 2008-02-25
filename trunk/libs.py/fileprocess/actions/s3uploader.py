d240db8333221961d4987eede2817fcf6870e7fe
justin <none@none>
2008-02-24 22:19:53 -0600

    [svn] Added S3 configuration options to the development.ini file and created a flag to make uploading optional
    --HG--
    branch : svn.rubiconmusicplayer.com

--- trunk/libs.py/fileprocess/actions/s3uploader.py
+++ trunk/libs.py/fileprocess/actions/s3uploader.py
@@ -2,21 +2,25 @@ import logging
 import S3
 import os
 from baseaction import BaseAction
+from pylons import config
 
 log = logging.getLogger(__name__)
-
-AWS_ACCESS_KEY_ID = '17G635SNK33G1Y7NZ2R2'
-AWS_SECRET_ACCESS_KEY = 'PHDzFig4NYRJoKKW/FerfhojljL+sbNyYB9bEpHs'
-
-BUCKET = 'music.rubiconmusicplayer.com'
 
 READCHUNK = 1024 *128 #128k at a time, i think that's fair
 
 class S3Uploader(BaseAction):
     def process(self, file):
+        if config['S3_upload'] == 'false':
+            os.remove(file['fname'])
+            log.warn("Removed %s because S3_upload flag is set to false", 
+                file['fname']
+            )
+            return file
+
         conn = S3.AWSAuthConnection(
-            AWS_ACCESS_KEY_ID, 
-            AWS_SECRET_ACCESS_KEY)
+            config['S3_accesskey'], 
+            config['S3_secret']
+        )
         fo = open(file['fname'],'rb')
 
         data = ''
@@ -27,10 +31,14 @@ class S3Uploader(BaseAction):
             bytes = fo.read(readbytes)
         fo.close()
 
-        response=conn.put(BUCKET, file['sha'], data)
+        response=conn.put(config['S3_music_bucket'], file['sha'], data)
         if (response.message == '200 OK'):
             os.remove(file['fname'])
-            file['fname'] = '/'.join([conn.server, BUCKET, file['sha']])
+            file['fname'] = '/'.join([
+                conn.server, 
+                config['S3_music_bucket'], 
+                file['sha']
+            ])
             log.info("%s successfully uploaded to S3", file['title'])
         else:
             log.error(response.message)
