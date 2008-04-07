00ba49060a69b6583a9a0443f02b18eca2e7edf9
Brian Smith <ohohvi@gmail.com>
2008-04-07 01:04:49 -0500

    Added a test server switch for upload testing
    --HG--
    branch : trunk

--- uploader/config.py
+++ uploader/config.py
@@ -1,3 +1,7 @@
 local = dict(server_addr='127.0.0.1', server_port=2985)
+
 staging = dict(server_addr='stage.rubiconmusicplayer.com', server_port=80)
+
+test_server = dict(server_addr='rubiconmusicplayer.com', server_port=3425)
+
 current = staging
