b3473eda1939f561801b51449f7f7fc9dabcc822
Justin Tulloss <jmtulloss@gmail.com>
2008-04-17 15:56:59 -0500

    Made the taggetter try to figure out the totaltracks on an album given the tracknumber tag
    --HG--
    branch : trunk

--- libs.py/fileprocess/actions/taggetter.py
+++ libs.py/fileprocess/actions/taggetter.py
@@ -1,5 +1,6 @@
 import logging
 import hashlib, os
+import re, copy
 from mutagen.easyid3 import EasyID3
 from mutagen.mp3 import MP3, HeaderNotFoundError
 from baseaction import BaseAction
@@ -13,6 +14,12 @@ class TagGetter(BaseAction):
     file, which is pretty dangerous. We'll move it to a hopefully more reliable 
     musicbrainz based system when somebody codes that.
     """
+
+    def __init__(self, **kwargs):
+        super(TagGetter, self).__init__(**kwargs)
+
+        self.tracknum_strip = re.compile('[^0-9/]')
+        self.ttrack_find = re.compile('([0-9])*/?([0-9])*')
 
     def process(self, file):
         """This is rediculously easy with easyid3"""
@@ -31,6 +38,19 @@ class TagGetter(BaseAction):
         # I join the lists here for ease of processing later.
         for k in audio.keys():
             file[k] = ','.join(file[k])
+
+        # Extra tags that I can figure out
+        file['tracknumber'] = self.tracknum_strip.sub('', file['tracknumber'])
+
+        tparts = self.ttrack_find.findall(file['tracknumber'])[0]
+        oldtracknum = copy.copy(file['tracknumber'])
+        try:
+            file['tracknumber'] = int(tparts[0])
+            file['tracknumber'] = int(tparts[1])
+        except ValueError, e:
+            # Sometimes we don't have one of the values we were looking for
+            file['tracknumber'] = oldtracknum
+
         file['duration'] = int(audio.info.length*1000)
 
         audio.delete() #remove the ID3 tags, we don't care for them
