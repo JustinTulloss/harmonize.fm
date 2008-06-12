23f3df1742ba84c32f1c81d9cd97699cc2a55346
Brian Smith <brian@harmonize.fm>
2008-06-12 10:44:32 -0500

    Mac uploader now uses fingerprints instead of hashes
    --HG--
    branch : trunk

--- uploader/setup.py
+++ uploader/setup.py
@@ -1,17 +1,22 @@
 import sys
 from distutils.core import setup
+import os
 
 r_scripts = ['fb.py', 'dir_browser.py', 'upload.py', 'itunes.py', 'tags.py', 
-			 'config.py']
+			 'config.py', 'genpuid.py']
 
 if sys.platform == 'darwin':
+	if not os.path.exists('genpuid_bin_osx'):
+		print 'genpuid directory missing, download it at musicip.com'
 	import py2app
 	setup(
 		name='Harmonize',
 		setup_requires=['py2app'],		
 		app=['Harmonize_osx.py'],
 		scripts=r_scripts,
-		data_files=['MainMenu.nib']
+		data_files=['MainMenu.nib', 
+					'genpuid_bin_osx/genpuid', 'genpuid_bin_osx/AACTagReader',
+					'genpuid_bin_osx/mipcore']
 	)
 elif sys.platform == 'win32':
 	import py2exe
