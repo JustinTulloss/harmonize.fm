135cf3cbc15f329cd6d1c64389c280eebf21fb8b
Brian Smith <brian@harmonize.fm>
2008-06-29 22:55:28 -0500

    decorator module wasnt getting packaged correctly so Im just making a decorator manually.
    --HG--
    branch : trunk

--- uploader/excepthandler.py
+++ uploader/excepthandler.py
@@ -1,26 +1,26 @@
-from decorator import decorator
 import traceback as tb
 from StringIO import StringIO
 import sys
 import config
 
-@decorator
-def exception_managed(fn, *args, **kws):
-	try:
-		fn(*args, **kws)
-	except Exception, e:
-		f_contents = StringIO()
-		tb.print_tb(sys.exc_traceback, file=f_contents)
-		contents = f_contents.getvalue()
-		f_contents.close()
-
+def exception_managed(fn):
+	def wrapper(*args, **kws):
 		try:
-			conn = config.get_conn()
-			conn.request('POST', '/upload/error', contents, 
-							{'Content-Type':'text/plain'})
-			conn.getresponse().read()
-		except Exception:
-			return
+			fn(*args, **kws)
+		except Exception, e:
+			f_contents = StringIO()
+			tb.print_tb(sys.exc_traceback, file=f_contents)
+			contents = f_contents.getvalue()
+			f_contents.close()
 
-		if config.current['debug']:
-			import pdb; pdb.set_trace()
+			try:
+				conn = config.get_conn()
+				conn.request('POST', '/upload/error', contents, 
+								{'Content-Type':'text/plain'})
+				conn.getresponse().read()
+			except Exception:
+				return
+
+			if config.current['debug']:
+				import pdb; pdb.set_trace()
+	return wrapper
