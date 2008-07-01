479d652e20c4ad3cb198e92042908dbe3a363704
Dave Paola <dpaola2@gmail.com>
2008-07-01 15:24:18 -0500

    changed template a bit, still need to wait 48 hours to test on facebook outside the console
    --HG--
    branch : trunk

--- masterapp/masterapp/controllers/player.py
+++ masterapp/masterapp/controllers/player.py
@@ -314,8 +314,8 @@ class PlayerController(BaseController):
     def publish_to_facebook(self, spot):
         return '1'
         # oops, exceeded time window.  from now only only 10 times per 48 hours (moving)
-        title_t = '{actor} wrote a new Spotlight'
-        body_t = '<b>{title} by {author}</b><br>{comment}<br>Visit <a href="http://harmonize.fm">Harmonize.fm</a> to listen.'
-        body_d = '{"title":"'+ spot.title +'","author":"'+ spot.author +'","comment":"'+ spot.comment +'"}'
+        title_t = '{actor} created <fb:if-multiple-actors>Spotlights<fb:else>a Spotlight</fb:else></fb:if-multiple-actors> on <a href="http://harmonize.fm">Harmonize.fm</a>'
+        body_t = '<i><fb:pronoun uid="actor" useyou="false" capitalize="true" /> said: </i>{comment}  (<b>{title}</b> by {artist})'
+        body_d = '{"title":"'+ spot.title +'", "album":"'+ spot.title +'", "artist":"'+ spot.author +'", "comment":"'+ spot.comment +'"}'
         r = facebook.feed.publishTemplatizedAction(title_template=title_t, body_template=body_t, body_data=body_d)
         return r
