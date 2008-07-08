990a1218a591254ec81f2dd89acbc6e18bea59bd
Brian Smith <brian@harmonize.fm>
2008-07-08 13:52:35 -0500

    More exception handler fixes that should work on production servers.
    --HG--
    branch : trunk

--- masterapp/masterapp/controllers/upload.py
+++ masterapp/masterapp/controllers/upload.py
@@ -265,8 +265,13 @@ class UploadController(BaseController):
         if stack_trace == '':
             return '0'
         def sendmail():
+            if config['use_gmail'] == 'yes':
+                password = config['feedback_password']
+            else:
+                password = None
+
             mail(config['smtp_server'], config['smtp_port'],
-                config['feedback_email'], config['feedback_password'],
+                config['feedback_email'], password,
                 'brian@harmonize.fm', 'Uploader exception', stack_trace)
         thread.start_new_thread(sendmail, ())
         return '1'
