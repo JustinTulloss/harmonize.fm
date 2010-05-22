3eb99c2d9d97daf61724532394763edeb2c29509
Justin Tulloss <justin@harmonize.fm>
2010-05-22 15:20:23 -0700

    Updated to work with fabric 0.9
    --HG--
    branch : trunk

--- deployment/fabfile.py
+++ deployment/fabfile.py
@@ -1,21 +1,24 @@
-config.project = 'harmonize'
-config.repo = '/var/www/sites/repo/main'
-config.fab_hosts = ['harmonize.fm']
+from fabric.api import env
+from fabric.operations import sudo, run
+
+env['project'] = 'harmonize'
+env['repo'] = '/var/www/sites/repo/main'
+env['hosts'] = ['harmonize.fm']
 
 def produce():
     "Push changes to live server"
     run("echo Pushing changes to production!")
-    sudo('python $(repo)/deployment/produce.py')
+    sudo('python $(repo)s/deployment/produce.py' % env)
 
 def stage():
     "Push changes to staging server"
     run("echo Pushing changes to staging server")
-    sudo('python $(repo)/deployment/stage.py')
+    sudo('python %(repo)s/deployment/stage.py' % env)
 
 def stage_pipeline():
     "Push changes to the file_pipeline only"
-    sudo('python $(repo)/deployment/stage_pipeline.py')
+    sudo('python %(repo)s/deployment/stage_pipeline.py' % env)
 
 def produce_pipeline():
     "Push changes to the live file pipeline from stage"
-    sudo('python $(repo)/deployment/produce_pipeline.py')
+    sudo('python %(repo)s/deployment/produce_pipeline.py' % env)
