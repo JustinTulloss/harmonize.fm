0ef6261d3dd5eaf165473a3b39afb59b9452b8f1
Justin Tulloss <justin@harmonize.fm>
2010-05-22 15:22:30 -0700

    Removed deprecated things from create_build, make it independently executable."
    --HG--
    branch : trunk

--- uploader/create_build.py
+++ uploader/create_build.py
@@ -1,14 +1,18 @@
-from popen2 import popen2
+import subprocess
 import re
 
 def create():
     build_file = open('build.py', 'w+')
-    stdout, stdin = popen2('hg identify')
-    stdin.close()
+    p = subprocess.Popen(['hg', 'identify'], stdout=subprocess.PIPE)
+    stdout, stderr = p.communicate()
 
-    match = re.match('^[a-fA-F0-9]+', stdout.read())
+    match = re.match('^[a-fA-F0-9]+', stdout)
     if not match:
         raise Exception('Unable to determine build version')
 
     build_file.write('repo_version = "%s"' % match.group(0))
+    print "Version is: " + match.group(0)
     build_file.close()
+
+if __name__ == '__main__':
+    create()
